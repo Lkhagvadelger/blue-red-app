005266e01c5a1520e208837ad05a72f2bcfa6169
Lkhagvadelger <lkhagvadelger@medervahealth.com>
2026-04-08 17:17:25 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'services/storage_service.dart';
 import 'services/review_service.dart';
@@ -7,6 +8,7 @@ import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   final box = await StorageService.init();
   await ReviewService.recordSessionStart();
 
